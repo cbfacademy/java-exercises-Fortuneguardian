ec8e5a621d83bbd10b42ab7ab4916b99ed7e0030
Jigalee Puff <marsayah@gmail.com>
2023-10-26 19:41:07 +0100

    pass the dutchie

--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -7,14 +7,30 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@DisplayName(value = "Basic Test Suite")
-public class AppTest {
+@DisplayName(value = "Leap Year should return")
+public class LeapYearTests {
 
-    @Test
-    @DisplayName("creates the app")
-    public void createsAnApp() {
-        final App app = new App();
+  static Stream<Arguments> InputandOutputProvider() {
+    return Stream.of(
+        arguments(1600, true),
+        arguments(1700, false).
+        arguments(1800, false),
+        arguments(1900, false),
+        arguments(2000, true),
+        arguments(2001, false),
+        arguments(2002, false).
+        arguments(2003, false),
+        arguments(2004, true), 
+        arguments(2005, false),
+        arguments(2006, false),
+        arguments(2007, false),
+        arguments(2008, true),
+        arguments(2009, false),
+        arguments(2010, false),
+        arguments(2020, true), 
+        arguments(2021, false),
+        arguments(2022, false)
 
-        assertThat(app, is(notNullValue()));
-    }
+    );
+  }
 }
